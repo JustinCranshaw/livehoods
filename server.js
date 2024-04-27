2b8add6de14560a086df47dbfa03cfe229183379
Justin Cranshaw <justin@cranshaw.me>
2024-04-27 15:14:52 -0700

    added Procfile

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 express()
-  .use(express.static(path.join(__dirname, './src')))  // Serves your project root as static
+  .use(express.static(path.join(__dirname, './docs')))  // Serves your project root as static
   .get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))  // Fallback to index.html
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
